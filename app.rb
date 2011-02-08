74470f1abce93fa5da08ed176a55a4274cfcbea4
Tom Wilson <tom@jackhq.com>
2011-02-07 19:25:25 -0500

    update site with awesome fast cache

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require 'sinatra'
 require 'haml'
 require 'yaml'
 
+before do
+  response.headers['Cache-Control'] = 'public, max-age=300000'
+end
+
+
 get '/' do
   @title = 'Team'
   @content = 'Jack Russell Software Company, is a Ruby on Rails Development shop, providing awesome mobile and web applications to HealthcareIT, we love testing, pairing and agile development.  We are constantly striving to master our craft of software development.'
