a89b4b8e96afbe4b4c4f9e7c7ceb494bea085d4c
Tom <tom@jackhq.com>
2016-01-29 11:03:53 -0500

    removed chs hackathon

--- app/community/local.js
+++ app/community/local.js
@@ -2,9 +2,6 @@ module.exports = [{
   name: 'Charleston Tech Slack',
   link: 'https://charlestontechslack.herokuapp.com/'
 }, {
-  name: 'Charleston Hackathon',
-  link: 'http://hackathon.sparcedge.com/'
-}, {
   name: 'Charleston BarCamp',
   link: 'http://barcampchs.org/'
 }, {
